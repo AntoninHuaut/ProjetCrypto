fc2eafb5d5bb08da42b0d8e87dbfdae2b0f6f2f9
Antonin Huaut <antonin.huaut+github@gmail.com>
2021-04-06 09:49:59 +0200

    comment

--- a51.py
+++ a51.py
@@ -8,6 +8,11 @@ lsfr3 = 0
 
 
 def xorLSFR1(hasDecalage):
+    """
+    Effectue le xor du registre LSFR1
+    :param hasDecalage: Indique si le registre doit être décalé
+    :return: b n°18
+    """
     global lsfr1
     b18 = getBitIndice(lsfr1, 18, lsfr1_longueur)
 
@@ -25,6 +30,11 @@ def xorLSFR1(hasDecalage):
 
 
 def xorLSFR2(hasDecalage):
+    """
+    Effectue le xor du registre LSFR2
+    :param hasDecalage: Indique si le registre doit être décalé
+    :return: b n°21
+    """
     global lsfr2
     b21 = getBitIndice(lsfr2, 21, lsfr2_longueur)
 
@@ -40,6 +50,11 @@ def xorLSFR2(hasDecalage):
 
 
 def xorLSFR3(hasDecalage):
+    """
+    Effectue le xor du registre LSFR3
+    :param hasDecalage: Indique si le registre doit être décalé
+    :return: b n°22
+    """
     global lsfr3
     b22 = getBitIndice(lsfr3, 22, lsfr3_longueur)
 
@@ -57,10 +72,22 @@ def xorLSFR3(hasDecalage):
 
 
 def getMajoriteBitOrange(bLsfr1, bLsfr2, bLsfr3):
+    """
+    Renvoie le bit majoritaire à l'emplacement orange des trois registres
+    :param bLsfr1: Bit orange LSFR1
+    :param bLsfr2: Bit orange LSFR2
+    :param bLsfr3: Bit orange LSFR3
+    :return: Bit (1 ou 0)
+    """
     return 1 if bLsfr1 + bLsfr2 + bLsfr3 > 1 else 0
 
 
 def xorGlobal(nb):
+    """
+    Effectue le xor global du système
+    :param nb: Nombre de xor à effectuer
+    :return: Tableau de bits de résultat des xor
+    """
     resBits = []
 
     for i in range(0, nb):
@@ -80,12 +107,17 @@ def xorGlobal(nb):
     return resBits
 
 
-def initXorRegistres(array):
-    firstVal = int(array[0], 2)
+def initXorRegistres(key):
+    """
+    Initialise les registres du système via les xor
+    :param key: Clé secrète
+    :return: Nouvelle clé secrète
+    """
+    firstVal = int(key[0], 2)
     newVals = ""
 
-    for i in range(0, len(array)):
-        newVals += str(int(array[i], 2) ^ firstVal)
+    for i in range(0, len(key)):
+        newVals += str(int(key[i], 2) ^ firstVal)
 
     xorLSFR1(True)
     xorLSFR2(True)
@@ -95,6 +127,10 @@ def initXorRegistres(array):
 
 
 def initRegistre(key):
+    """
+    Initialise les registres du système
+    :param key: Clé secrète
+    """
     global lsfr1, lsfr2, lsfr3
 
     key = initXorRegistres(key)
@@ -113,24 +149,53 @@ def initRegistre(key):
 
 
 def decalage(binary, toAdd, length):
+    """
+    Effectue un décalage de registre vers la gauche
+    :param binary: Valeur binaire du registre
+    :param toAdd: Bit à ajouter à droite
+    :param length: Taille du registre
+    :return: Nouveau registre
+    """
     binStr = format(binary, 'b').zfill(length)
     binStr += str(toAdd)
     return int(binStr[1:], 2)
 
 
-def affichageRegistre(registre, longueur):
+def afficheRegistre(registre, longueur):
+    """
+    Affiche un registre
+    :param registre: Valeur du registre
+    :param longueur: Longueur du registre
+    """
     print(lsfr1, "=>", format(registre, 'b').zfill(longueur))
 
 
 def getBitIndice(numberValue, index, longueur):
+    """
+    Récupère le bit à l'indice demandé
+    :param numberValue: Valeur du registre
+    :param index: Indice du bit à récupérer
+    :param longueur: Longueur du registre
+    :return: Bit à l'indice du registre
+    """
     return int(format(numberValue, 'b').zfill(longueur)[longueur - (index + 1)])
 
 
 def getLettreBinaire(lettre):
+    """
+    Récupère une lettre binaire
+    :param lettre: Lettre
+    :return: Valeur de la lettre
+    """
     return str(''.join(format(ord(el), 'b') for el in lettre))
 
 
 def getPhraseBinaire(input):
+    """
+    Récupère une phrase en binaire
+    :param input: Phrase à récupérer
+    :return: Tableau contenant chaque valeur de lettre
+    """
     motConvert = ""
     listBinaires = []
 
@@ -144,6 +209,11 @@ def getPhraseBinaire(input):
 
 
 def chiffrerMessage(message):
+    """
+    Chiffre un message
+    :param message: Message à chiffrer
+    :return: Message chiffré
+    """
     messageChiffre = ""
     messageBinaire = getPhraseBinaire(message)
     xorResult = xorGlobal(len(messageBinaire))
@@ -155,6 +225,11 @@ def chiffrerMessage(message):
 
 
 def convertirMessageBinaire(messageBinaire):
+    """
+    Converti un message binaire en message lisible
+    :param messageBinaire: Message binaire
+    :return: Message lisible
+    """
     messageConvertie = ""
 
     for i in range(0, len(messageBinaire), 8):
@@ -165,6 +240,11 @@ def convertirMessageBinaire(messageBinaire):
 
 
 def dechifferMessage(messageChiffre):
+    """
+    Déchiffre un message
+    :param messageChiffre: Message chiffré
+    :return: Message lisible
+    """
     message = ""
     xorResult = xorGlobal(len(messageChiffre))
 
